4eb9d261e48c18d02fc4783ca35da8e433ac727d
Aung-Seth-Pai01 <114895232+Aung-Seth-Pai01@users.noreply.github.com>
2023-10-20 01:00:49 +0630

    receive message ok

--- General/chatbot_test/botwork.py
+++ General/chatbot_test/botwork.py
@@ -1,4 +1,5 @@
 import requests
+import random
 
 def is_user_message(message):
     """Check if the message is a message from the user"""
@@ -12,32 +13,17 @@ def send_message(recipient_id, response, bot):
     bot.send_text_message(recipient_id, response)
     return "success"
 
-
-# def send_message(recipient_id, text, PAGE_ACCESS_TOKEN, FB_API_URL):
-#     """Send a response to Facebook"""
-#     payload = {
-#         'message': {
-#             'text': text
-#         },
-#         'recipient': {
-#             'id': recipient_id
-#         },
-#         'notification_type': 'regular'
-#     }
-#     auth = {
-#         'access_token': PAGE_ACCESS_TOKEN
-#     }
-#     response = requests.post(
-#         FB_API_URL,
-#         params=auth,
-#         json=payload
-#     )
-#     return response.json()
-
 def get_bot_response(message):
     """This is just a dummy function, returning a variation of what
     the user said. Replace this function with one connected to chatbot."""
     return "This is a dummy response to '{}'".format(message)
+
+
+#chooses a random message to send to the user
+def get_message():
+    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
+    # return selected item to the user
+    return random.choice(sample_responses)
 
 def respond(recipient_id, message, bot):
     """Formulate a response to the user and
--- General/chatbot_test/server.py
+++ General/chatbot_test/server.py
@@ -4,7 +4,7 @@ import random
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-from botwork import is_user_message, send_message,respond
+from botwork import is_user_message, get_message, send_message, respond
 
 from pymessenger.bot import Bot
 
@@ -13,10 +13,9 @@ load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 
-FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages'
+# FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages'
 VERIFY_TOKEN = os.environ.get("VERIFY_TOKEN")
 PAGE_ACCESS_TOKEN = os.environ.get("PAGE_ACCESS_TOKEN")
-print(PAGE_ACCESS_TOKEN)
 bot = Bot(PAGE_ACCESS_TOKEN)
 
 # # test server
@@ -30,38 +29,51 @@ def verify_fb_token(req):
     else:
         return 'Invalid verification token'
 
-#chooses a random message to send to the user
-def get_message():
-    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
-    # return selected item to the user
-    return random.choice(sample_responses)
+
+
+@app.route("/policy", methods=["GET"])
+def policy():
+    return "THIS IS TEST POLICY"
 
 @app.route("/webhook", methods=["GET", "POST"])
 def listen():
     """This is the main function flask uses to 
     listen at the `/webhook` endpoint"""
     if request.method == 'GET':
+        print("Authenticating.....")
         return verify_fb_token(request)
 
     if request.method == 'POST':
-        payload = request.get_json()
+        payload = request.get_json() # object and entry as keys of payload
+        print(payload)
         for event in payload['entry']:
-            messaging = event['messaging']
-            for message in messaging:
-                if message.get('message'):
-                    #Facebook Messenger ID for user so we know where to send response back to
-                    recipient_id = message['sender']['id']
-                    if message['message'].get('text'):
-                        if is_user_message(message):
-                            text = message['message']['text']
-                            respond(recipient_id, text, bot)
-                    #if user sends us a GIF, photo,video, or any other non-text item
-                    if message['message'].get('attachments'):
-                        response_sent_nontext = get_message()
-                        respond(recipient_id, response_sent_nontext, bot)      
+            # if user sent message
+            if "messaging" in event.keys():
+                
+                messaging = event['messaging']
+                for message in messaging:
+                    if message.get('message'):
+                        #Facebook Messenger ID for user so we know where to send response back to
+                        recipient_id = message['sender']['id']
+                        # if user sends text
+                        if message['message'].get('text'):
+                            if is_user_message(message):
+                                text = message['message']['text']
+                                print(text)
+                                # respond(recipient_id, text, bot)   
+                                # bot.send_text_message(recipient_id, text)
+                                return "OK"          
+                        #if user sends us a GIF, photo,video, or any other non-text item
+                        if message['message'].get('attachments'):
+                            print("MESSAGE IS NON_TEXT")
+                            return "NON TEXT"
+                            # response_sent_nontext = get_message()
+                            # respond(recipient_id, response_sent_nontext, bot)
+            else:
+                pass  
         return "ok"
     
 
 # To locally run the app
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=5000)
+    app.run(debug=False, host='0.0.0.0',port=5000)
